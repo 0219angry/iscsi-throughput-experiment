720cbd76c0a88a0fefab478cd273923c2de3ce1b
tsuyukusayu <angry.yuma.0219@gmail.com>
2025-02-06 15:59:36 +0900

    UPDATE main.c

---
+++ src/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <poll.h>
+
+#include "iscsi.h"
+#include "scsi-lowlevel.h"
+
+
+
+int main(int argc, char const *argv[]) {
+  struct iscsi_context *iscsi;
+  struct pollfd pfd;
+
+  exit(EXIT_SUCCESS);
+}
