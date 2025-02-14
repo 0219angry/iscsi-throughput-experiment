c3c6651308cf8d3a067da60d1dabb62f30e48876
0219Angry <angry.yuma.0219@gmail.com>
2025-02-15 00:09:09 +0900

    ADD LOG() logging macro

---
+++ include/log.h
@@ -0,0 +1,17 @@
+// log.h
+#ifndef LOG_H
+#define LOG_H
+
+#include <stdio.h>
+#include <time.h>
+
+#define LOG(fmt, ...) do { \
+    time_t t = time(NULL); \
+    struct tm *tm_info = localtime(&t); \
+    char time_buf[20]; \
+    strftime(time_buf, sizeof(time_buf), "%Y-%m-%d %H:%M:%S", tm_info); \
+    fprintf(stdout, "[%s] %s:%d (%s) " fmt "\n", \
+        time_buf, __FILE__, __LINE__, __func__, ##__VA_ARGS__); \
+} while (0)
+
+#endif // LOG_H
